a0468b77664326b0b7bc68020faa899a039cd5f8
KauanMO <kauaanmatheus@gmail.com>
2025-07-17 11:47:09 -0300

    feat: scheduler to train brain

--- brain/main.py
+++ brain/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import service
 from schemas import EtaRequest, EtaResponse, EvaluateModelResponse
 import train_brain
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
 
 app = FastAPI()
 
@@ -17,6 +19,9 @@ def evaluate_model():
 
 if __name__ == "__main__":
     import uvicorn
-    
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(train_brain.train_model, 'cron', hour=0, minute=0)
+    scheduler.start()
+
     train_brain.train_model()
     uvicorn.run("main:app", host="0.0.0.0", port=5500, reload=True)
